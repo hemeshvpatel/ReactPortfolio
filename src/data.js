d1197c4abe5c187672bf70322475082719188b66
hemesh <hemesh.v.patel@gmail.com>
2021-09-02 15:54:37 -0500

    added more data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,8 @@
-export const userData = { firstName: "Luke", lastName: "Skywalker" };
+export const userData = {
+  firstName: "Luke",
+  lastName: "Skywalker",
+  location: "Austin, TX",
+};
 
 export const roles = [
   "Software Engineer",
@@ -6,13 +10,22 @@ export const roles = [
   "Back End Developer",
 ];
 
-export const technologies = ["React.js", "Node.js", "HTML", " CSS"];
+export const technologies = [
+  "Javascript",
+  "React.js",
+  "Node.js",
+  "HTML",
+  "CSS",
+  "Ruby",
+];
 
 export const about = [
   "Developer and React.js lover.",
   "Passionate about how things work and how technology can help solve problems.",
   "My career has led me through roles in Finance, Operations, and most recently Web Development.",
 ];
+
+//Can get data from: https://www.qwikresume.com/resume-samples/junior-software-developer/#previewResume
 
 //Order from most recent to oldest
 export const workExperience = [
@@ -22,7 +35,16 @@ export const workExperience = [
     position: "Senior Front-end Engineer",
     startDate: "1/1/2020",
     endDate: "12/31/2020",
-    bullets: [""],
+    summary:
+      "Senior Software Developer with 8 plus years of experience in the Engineering domain is seeking to apply and expand my knowledge and skills towards working in a collaborative environment to develop quality software solutions that address and solve business problems.",
+    bullets: [
+      "Working closely with the QA and Product Management teams as well as other developers to ensure quality and punctual software development.",
+      "Participating in daily scrum meetings, sprint planning, unit testing and bug fixes.",
+      "Developing and maintaining a reporting system for support users, managers and executives to keep track of support calls both on a moment to moment basis and over time using PHP, MySQL, HTML, and CSS.",
+      "Assisting the Senior Developer with the design, development, deployment, and maintenance of all menu maintenance portal code using PHP, MySQL, HTML, CSS, and Javascript.",
+      "The menu maintenance portal is a web-based system that allows users to manage all of the settings for each of their restaurants in one place.",
+      "Troubleshooting server communication and any potential Cron, SMTP or POP3 issues.",
+    ],
   },
   {
     company: "Uber",
@@ -30,6 +52,15 @@ export const workExperience = [
     position: "Front-end Developer",
     startDate: "1/1/2019",
     endDate: "12/31/2019",
+    summary:
+      "Senior with 7 plus years of experience in the Engineering domain is seeking to apply and expand my knowledge and skills towards working in a collaborative environment to develop quality software solutions that address and solve business problems.",
+    bullets: [
+      "In charge of all content deployment for digital menu boards and management training systems.",
+      "Participating in daily scrum meetings, sprint planning, unit testing and bug fixes.",
+      "Developing and maintaining a reporting system for support users, managers and executives to keep track of support calls both on a moment to moment basis and over time using PHP, MySQL, HTML, and CSS.",
+      "Assisting the Senior Developer with the design, development, deployment, and maintenance of all menu maintenance portal code using PHP, MySQL, HTML, CSS, and Javascript.",
+      "The menu maintenance portal is a web-based system that allows users to manage all of the settings for each of their restaurants in one place.",
+    ],
   },
   {
     company: "Twitter",
@@ -37,6 +68,14 @@ export const workExperience = [
     position: "Software Engineer",
     startDate: "1/1/2018",
     endDate: "12/31/2018",
+    summary:
+      "Software Developer with 5 plus years of experience in the Engineering domain is seeking to apply and expand my knowledge and skills towards working in a collaborative environment to develop quality software solutions that address and solve business problems.",
+    bullets: [
+      "Participating in daily scrum meetings, sprint planning, unit testing and bug fixes.",
+      "Developing and maintaining a reporting system for support users, managers and executives to keep track of support calls both on a moment to moment basis and over time using PHP, MySQL, HTML, and CSS.",
+      "Assisting the Senior Developer with the design, development, deployment, and maintenance of all menu maintenance portal code using PHP, MySQL, HTML, CSS, and Javascript.",
+      "The menu maintenance portal is a web-based system that allows users to manage all of the settings for each of their restaurants in one place.",
+    ],
   },
   {
     company: "Netflix",
@@ -44,6 +83,14 @@ export const workExperience = [
     position: "Associate Software Engineer",
     startDate: "1/1/2017",
     endDate: "12/31/2017",
+    summary:
+      "Junior Software Developer with 3 plus years of experience in the Engineering domain is seeking to apply and expand my knowledge and skills towards working in a collaborative environment to develop quality software solutions that address and solve business problems.",
+    bullets: [
+      "Participating in daily scrum meetings, sprint planning, unit testing and bug fixes.",
+      "Developing and maintaining a reporting system for support users, managers and executives to keep track of support calls both on a moment to moment basis and over time using PHP, MySQL, HTML, and CSS.",
+      "Assisting the Senior Developer with the design, development, deployment, and maintenance of all menu maintenance portal code using PHP, MySQL, HTML, CSS, and Javascript.",
+      "The menu maintenance portal is a web-based system that allows users to manage all of the settings for each of their restaurants in one place.",
+    ],
   },
   {
     company: "Facebook",
@@ -51,6 +98,14 @@ export const workExperience = [
     position: "Technical Support Engineer - Enterprise",
     startDate: "1/1/2016",
     endDate: "12/31/2016",
+    summary:
+      "Technical Support Engineer with 1 plus years of experience in the Engineering domain is seeking to apply and expand my knowledge and skills towards working in a collaborative environment to develop quality software solutions that address and solve business problems.",
+    bullets: [
+      "Developing and maintaining a reporting system for support users, managers and executives to keep track of support calls both on a moment to moment basis and over time using PHP, MySQL, HTML, and CSS.",
+      "Participating in daily scrum meetings, sprint planning, unit testing and bug fixes.",
+      "Assisting the Senior Developer with the design, development, deployment, and maintenance of all menu maintenance portal code using PHP, MySQL, HTML, CSS, and Javascript.",
+      "The menu maintenance portal is a web-based system that allows users to manage all of the settings for each of their restaurants in one place.",
+    ],
   },
   {
     company: "Apple",
@@ -58,5 +113,13 @@ export const workExperience = [
     position: "Technical Support Engineer",
     startDate: "1/1/2015",
     endDate: "12/31/2015",
+    summary:
+      "Technical Support Engineer seeking to apply and expand my knowledge and skills towards working in a collaborative environment to develop quality software solutions that address and solve business problems.",
+    bullets: [
+      "The menu maintenance portal is a web-based system that allows users to manage all of the settings for each of their restaurants in one place.",
+      "Participating in daily scrum meetings, sprint planning, unit testing and bug fixes.",
+      "Developing and maintaining a reporting system for support users, managers and executives to keep track of support calls both on a moment to moment basis and over time using PHP, MySQL, HTML, and CSS.",
+      "Assisting the Senior Developer with the design, development, deployment, and maintenance of all menu maintenance portal code using PHP, MySQL, HTML, CSS, and Javascript.",
+    ],
   },
 ];
