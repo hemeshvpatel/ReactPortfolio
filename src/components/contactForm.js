af90f5a3821a4c8927d384362640109251b15a31
Hemesh Patel <hemesh.v.patel@gmail.com>
2021-09-03 12:34:49 -0500

    updated contact form headers, displayed bullets and summary for work experience

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function ContactForm() {
+  //Should this be a modal so when you click email/contact icon, this pop ups?
   return (
     <div
       style={{
@@ -13,15 +14,15 @@ export default function ContactForm() {
       <form name="contact" method="post">
         <input type="hidden" name="form-name" value="contact" />
         <p>
-          <label htmlFor="name">Name</label> <br />
+          <label htmlFor="name">Your Name...please</label> <br />
           <input type="text" id="name" name="name" required />
         </p>
         <p>
-          <label htmlFor="email">Email</label> <br />
-          <input type="email" id="email" name="email" required />
+          <label htmlFor="email">Email...only if you want</label> <br />
+          <input type="email" id="email" name="email" />
         </p>
         <p>
-          <label htmlFor="message">Message</label> <br />
+          <label htmlFor="message">Message...much appreciated!</label> <br />
           <textarea id="message" name="message" required></textarea>
         </p>
         <p>
