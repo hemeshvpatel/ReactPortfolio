5e74dcccad172311ad28849407863c6980d207ca
hemesh <hemesh.v.patel@gmail.com>
2021-09-03 07:04:10 -0500

    updated form for testing

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -11,39 +11,32 @@ export default function ContactForm() {
   }, []);
 
   return (
-    // <div
-    //   style={{
-    //     display: "flex",
-    //     alignItems: "center",
-    //     margin: "25px",
-    //     flexDirection: "column",
-    //   }}
-    // >
-    <Container maxWidth="sm">
-      <h2>CONTACT</h2>
-      {success && <p style={{ color: "green" }}>Thanks for your message! </p>}
-      <Card>
-        <form
-          name="contact"
-          method="POST"
-          action="/contact/?success=true"
-          data-netlify="true"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <TextField id="standard-basic" label="name" name="name" /> <br />
-          <TextField id="standard-basic" label="email" name="email" /> <br />
-          <TextField
-            multiline
-            id="standard-multiline-static"
-            label="message"
-            name="message"
-          />
-          <br />
-          <div>
-            <Button type="submit">Send</Button>
-          </div>
-        </form>
-      </Card>
-    </Container>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        margin: "25px",
+        flexDirection: "column",
+      }}
+    >
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label htmlFor="name">Name</label> <br />
+          <input type="text" id="name" name="name" required />
+        </p>
+        <p>
+          <label htmlFor="email">Email</label> <br />
+          <input type="email" id="email" name="email" required />
+        </p>
+        <p>
+          <label htmlFor="message">Message</label> <br />
+          <textarea id="message" name="message" required></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Submit message" />
+        </p>
+      </form>
+    </div>
   );
 }
