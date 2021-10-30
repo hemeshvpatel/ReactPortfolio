dcdc3f0909db213ca03ceb9e137e7f3fb06d1815
hemesh <hemesh.v.patel@gmail.com>
2021-10-30 09:50:53 -0500

    profile pic fix

--- src/components/About.js
+++ src/components/About.js
@@ -54,7 +54,7 @@ const Sections = styled.div`
 
 const ProfilePicture = styled.img`
   border-radius: 50% 50%;
-  width: 25vw;
+  width: 25svw;
   height: auto;
 
   @media screen and (max-width: 600px) {
